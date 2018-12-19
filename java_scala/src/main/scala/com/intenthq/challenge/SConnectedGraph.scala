7948589e14e586ca2c500fc1d34562117762fa73
theodickson <theo@Theodores-MacBook-Pro-2.local>
2018-12-19 07:54:49 +0000

    Saved a solution for connected graph
    (Just so I can pull it up at work after doing it at home)

--- java_scala/src/main/scala/com/intenthq/challenge/SConnectedGraph.scala
+++ java_scala/src/main/scala/com/intenthq/challenge/SConnectedGraph.scala
@@ -1,6 +1,15 @@
 package com.intenthq.challenge
 
-case class Node(value: Int, edges: List[Node] = List.empty)
+import scala.collection.mutable.Set
+
+case class Node(value: Int, edges: List[Node] = List.empty) {
+
+  override def equals(obj: Any) = obj match {
+    case Node(v,_) => value == v
+    case _ => false
+  }
+
+}
 
 object SConnectedGraph {
 
@@ -14,6 +23,19 @@ object SConnectedGraph {
   // run(a, b) == true
   // run(a, c) == true
   // run(b, d) == false
-  def run(source: Node, target: Node): Boolean = ???
+  def run(source: Node, target: Node): Boolean =
+    if (source == target) true
+    else {
+      source.edges.exists(src => run(src,target))
+    }
+
+  def runCyclic(source: Node, target: Node, visited: Set[Node] = Set.empty): Boolean =
+    //why do we use a set here?
+    if (visited.contains(source)) false
+    else if (source == target) true
+    else {
+      visited += source //what would happen if an immutable set was used?
+      source.edges.exists(runCyclic(_,target,visited))
+    }
 
 }
