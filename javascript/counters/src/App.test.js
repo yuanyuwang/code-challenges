be9e7f00d44c140b2760fd9f4d57107f88857cdb
Javier Pedreira <javier.pedreira@intenthq.com>
2021-07-30 16:25:05 +0200

    Fix broken test

--- javascript/counters/src/App.test.js
+++ javascript/counters/src/App.test.js
@@ -3,7 +3,7 @@ import { render } from "@testing-library/react";
 import App from "./App";
 
 test("should show 3 initial 0s", () => {
-  const { getAllByText, debug } = render(<App />);
+  const { getAllByText } = render(<App />);
   const linkElement = getAllByText("0");
-  expect(linkElement.length == 3);
+  expect(linkElement.length).toEqual(3);
 });
